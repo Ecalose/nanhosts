f7a353bf22674a884562e2e7557547971f900866
Ecalose <47114714+Ecalose@users.noreply.github.com>
2020-12-25 12:56:56 +0800

    Update fetch_ips.py

--- fetch_ips.py
+++ fetch_ips.py
@@ -24,7 +24,7 @@ RAW_URL = [
 
 IPADDRESS_PREFIX = ".ipaddress.com"
 
-HOSTS_TEMPLATE = """# GitHub520 Host Start
+HOSTS_TEMPLATE = """# Nan Host Start
 {content}# Star me GitHub url:https://github.com/Ecalose/nanhosts
 # 南+ Host End\n"""
 
